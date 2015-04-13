f2dd24649157dd27b9ca15bcdea4a6093f48f512
nebelwolfi <dr.backdooring@gmail.com>
2015-04-13 11:07:24 +0200

    asdf

--- src/Lulu.lua
+++ src/Lulu.lua
@@ -1,6 +1,6 @@
 if myHero.charName ~= "Lulu" then return end
 
-local version = 0.15
+local version = 0.17 -- REMEMBER: UPDATE .version FILE ASWELL FOR IN-GAME PUSH!
 local AUTO_UPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/nebelwolfi/scripts/master/src/Lulu.lua".."?rand="..math.random(1,10000)
@@ -46,7 +46,24 @@ local Spell = { Q = {Range = 925, Width = 50, Speed = 1450, Delay = 0.25},
                 W = {Range = 650},  
                 E = {Range = 650},
                 R = {Range = 900},}
-InterruptList = {"CaitlynAceintheHole", "Crowstorm", "DrainChannel", "GalioIdolOfDurand", "KatarinaR", "InfiniteDuress", "AbsoluteZero", "MissFortuneBulletTime", "AlZaharNetherGrasp"}
+local InterruptList = {
+    { charName = "Caitlyn", spellName = "CaitlynAceintheHole"},
+    { charName = "FiddleSticks", spellName = "Crowstorm"},
+    { charName = "FiddleSticks", spellName = "DrainChannel"},
+    { charName = "Galio", spellName = "GalioIdolOfDurand"},
+    { charName = "Karthus", spellName = "FallenOne"},
+    { charName = "Katarina", spellName = "KatarinaR"},
+    { charName = "Lucian", spellName = "LucianR"},
+    { charName = "Malzahar", spellName = "AlZaharNetherGrasp"},
+    { charName = "MissFortune", spellName = "MissFortuneBulletTime"},
+    { charName = "Nunu", spellName = "AbsoluteZero"},
+    { charName = "Pantheon", spellName = "Pantheon_GrandSkyfall_Jump"},
+    { charName = "Shen", spellName = "ShenStandUnited"},
+    { charName = "Urgot", spellName = "UrgotSwap2"},
+    { charName = "Varus", spellName = "VarusQ"},
+    { charName = "Warwick", spellName = "InfiniteDuress"}
+}
+local ToInterrupt = {}
 EnemyMinions = minionManager(MINION_ENEMY, Spell.Q.Range, myHero, MINION_SORT_MAXHEALTH_DEC)
 JungleMinions = minionManager(MINION_JUNGLE, Spell.Q.Range, myHero, MINION_SORT_MAXHEALTH_DEC)
 
@@ -58,36 +75,33 @@ enemies = GetEnemyHeroes()
 -- called once when the script is loaded
 function OnLoad()
     VP = VPrediction()
+    STS   = SimpleTS()
     IgniteSet()
     LoadMenu()
     --PrintChat ("<font color='#4ECB65'>Lulu v" .. tostring(version) .. " - loaded successful!</font>")
 end
 
 function LoadMenu()
-    menu = scriptConfig("Lulu script", "Lulu")
+    menu = MenuWrapper(player.charName, "unique" .. player.charName:gsub("%s+", ""))
+    
+    menu:SetTargetSelector(STS)
+    --menu:SetOrbwalker(Orb)
+    menu = menu:GetHandle()
     menu:addSubMenu("AutoCombo Settings", "combosettings")
-    menu.combosettings:addParam("useflashr","Use Flash+R", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+    menu.combosettings:addParam("useflashr","Save with Flash+R", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("B"))
     menu.combosettings:addParam("useq", "Use Q", SCRIPT_PARAM_ONOFF, true)
     menu.combosettings:addParam("usew", "Use W", SCRIPT_PARAM_ONOFF, true)
     menu.combosettings:addParam("usee", "Use E", SCRIPT_PARAM_ONOFF, true)
-    menu.combosettings:addParam("user", "Use R", SCRIPT_PARAM_ONOFF, true)
     
     menu:addSubMenu("AutoR Settings", "growthsettings")
-    menu.growthsettings:addParam("user","Use R", SCRIPT_PARAM_ONOFF, true)
+    menu.growthsettings:addParam("user","Auto use R", SCRIPT_PARAM_ONOFF, true)
+    menu.growthsettings:addParam("autorper", "Auto R ally under health %", SCRIPT_PARAM_SLICE, 30, 1, 100, 0)
     menu.growthsettings:addParam("smartr","Use if x enemies hitable", SCRIPT_PARAM_SLICE, 3, 1, 5, 0)
     
-    menu:addSubMenu("Custom Save Priority","custompriority")
-    --[[ old 
-    menu.custompriority: addParam(allie[1].charName, SCRIPT_PARAM_SLICE, 3, 1, 9, 0)
-    menu.custompriority: addParam(allie[2].charName, SCRIPT_PARAM_SLICE, 3, 1, 9, 0)
-    menu.custompriority: addParam(allie[3].charName, SCRIPT_PARAM_SLICE, 3, 1, 9, 0)
-    menu.custompriority: addParam(allie[4].charName, SCRIPT_PARAM_SLICE, 3, 1, 9, 0)
-    ]]--
+    menu.growthsettings:addSubMenu("Save Priority","custompriority")
     Boot()
     
-    menu:addParam("combokey", "Combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
     menu:addSubMenu("Harass Settings", "harass")
-    menu.harass:addParam("harass", "Toogle Auto Harass", SCRIPT_PARAM_ONKEYTOGGLE, false, string.byte("C"))
     menu.harass:addParam("mana","Auto Harass till Mana is under",SCRIPT_PARAM_SLICE, 30, 0, 101, 0)
     menu.harass:addParam("useq", "Use Q", SCRIPT_PARAM_ONOFF, true)
     menu.harass:addParam("usew", "Use W", SCRIPT_PARAM_ONOFF, true)
@@ -95,26 +109,23 @@ function LoadMenu()
     
     menu:addSubMenu("Draw Ranges", "drawab")
     menu.drawab:addParam("drawq", "Draw Q", SCRIPT_PARAM_ONOFF, true)
-    menu.drawab:addParam("draww", "Draw W", SCRIPT_PARAM_ONOFF, true)
-    menu.drawab:addParam("drawe", "Draw E", SCRIPT_PARAM_ONOFF, true)
+    menu.drawab:addParam("draww", "Draw W&E", SCRIPT_PARAM_ONOFF, true)
     menu.drawab:addParam("drawr", "Draw R", SCRIPT_PARAM_ONOFF, true)
-    if VIP_USER then
-    menu:addSubMenu("Packets", "Packets")
-    menu.Packets:addParam("QPACK", "Q Packets", SCRIPT_PARAM_ONOFF, false)
-    menu.Packets:addParam("EPACK", "E Packets", SCRIPT_PARAM_ONOFF, false)
-    end
+   
+    menu:addParam("interrupt", "Interrupt with W", SCRIPT_PARAM_ONOFF, true)
+    
     menu:addSubMenu("Orbwalker", "orbi")
     Orb:LoadToMenu(menu.orbi)
-    menu:addParam("info", " >> Version ", SCRIPT_PARAM_INFO, version)
+    
+    
+    menu:addParam("combokey", "Combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+    menu:addParam("harasskey", "Hold for Harass", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
+    menu:addParam("harasstoggle", "Toggle for Harass", SCRIPT_PARAM_ONKEYTOGGLE, false, string.byte("T "))
+    
+    menu:addParam("info2", " >> Version ", SCRIPT_PARAM_INFO, version)
 end
 
 function Boot()
-  ts1 = TargetSelector(TARGET_LESS_CAST_PRIORITY, 950, DAMAGE_MAGIC, nil, false)
-  ts1.name = "Ally"
-  menu.custompriority:addTS(ts1)
-  ts2 = TargetSelector(TARGET_LESS_CAST_PRIORITY, 950)
-  ts2.name = "Enemy"
-  menu.custompriority:addTS(ts2)
   enemyHeros = {}
   enemyHerosCount = 0
     for i = 1, heroManager.iCount do
@@ -123,6 +134,18 @@ function Boot()
       local enemyCount = enemyHerosCount + 1
       enemyHeros[enemyCount] = {object = hero, q = 0, w = 0, e = 0, r = 0, dfg = 0, ig = 0, myDamage = 0, manaCombo = 0}
       enemyHerosCount = enemyCount
+    end
+  end
+  menu.growthsettings.custompriority:addParam("info1"," >> Highest priority to save: ", SCRIPT_PARAM_INFO, 1)
+  alliedHeros = {}
+  alliedHerosCount = 0
+    for i = 1, heroManager.iCount do
+    local hero = heroManager:GetHero(i)
+    if hero.team == player.team then
+      local alliedCount = alliedHerosCount + 1
+      alliedHeros[alliedCount] = {object = hero, q = 0, w = 0, e = 0, r = 0, dfg = 0, ig = 0, myDamage = 0, manaCombo = 0}
+      alliedHerosCount = alliedCount
+      menu.growthsettings.custompriority:addParam("champ"..(alliedHerosCount-1),hero.charName,SCRIPT_PARAM_SLICE, 1, 1, 5, 0)
     end
   end
   BootDone = true
@@ -139,28 +162,19 @@ end
 
 -- handles script logic, a pure high speed loop
 function OnTick()
-  if not menu.Combo or myHero.dead or not bootDone then return end
+  if myHero.dead or not BootDone then return end
   Check()
-  Orb:EnableAttacks()
-  ts2:update()
-  target = ts2.target
-  if menu.Combo then
-    if ValidTarget(target, 900) and not target.dead then
-      Combo(target)
-    end
+  if menu.combokey then
+      Combo()
   end
   SmartR()
-  if menu.Harass then
-   if ValidTarget(target, 900) and not target.dead then
-      Harass(target)
-    end
+  if menu.harasskey or menu.harasstoggle then
+      Harass()
   end
 end
 
-function Combo(Target)
-  Orb:DisableAttacks()
-  
-  local Target = STS:GetTarget(Qrance)
+function Combo()
+  local Target = STS:GetTarget(Spell.Q.Range)
   if Target ~= nil then
     if menu.combosettings.usee and EReady then
       CastSpell(_E, Target)
@@ -172,16 +186,22 @@ function Combo(Target)
       end
     end
   end
-
-  Orb:EnableAttacks()
 end
 
 function SmartR()
   if RReady and menu.growthsettings.user then
+    if player.health < (player.maxHealth*(menu.growthsettings.autorper*0.01)) and countEnemy(player, 200) >= 1 then
+      CastSpell(_R, player)
+    end
+    for _, i in pairs (allies) do
+      if i.health < (i.maxHealth*(menu.growthsettings.autorper*0.01)) and countEnemy(i, 200) >= 1 then
+        CastSpell(_R, i)
+      end
+    end
     if countEnemy(player, 200) >= menu.growthsettings.smartr then
       CastSpell(_R, player)
     end
-    for _, i in pairs (allyHeroes) do
+    for _, i in pairs (allies) do
       if countEnemy(i, 200) >= menu.growthsettings.smartr and GetDistance(i, player) < Spell.R.Range then
         CastSpell(_R, i)
       end
@@ -189,9 +209,17 @@ function SmartR()
   end
 end
 
-function Harass(Target)
-  Orb:DisableAttacks()
+function countEnemy(allyHero, range)
+  local nearEnemy = 0
+  for i, e in pairs(enemies) do
+    if GetDistance(allyHero, e) < range then
+      nearEnemy = nearEnemy + 1
+    end
+  end
+  return nearEnemy
+end
 
+function Harass()
   local Target = STS:GetTarget(Spell.Q.Range+Spell.E.Range)
   if Target ~= nil then
     if GetDistance(Target, player) < Spell.Q.Range then
@@ -212,8 +240,6 @@ function Harass(Target)
       end
     end
   end
-  
-  Orb:EnableAttacks()
 end
 
 function SmartE(target)
@@ -239,10 +265,14 @@ function EnemysAround(Unit, range)
 end
 
 function OnProcessSpell(unit, spell)
-  if unit.isMe and spell.name:lower():find("attack") then
-    --PrintChat("Is attack!")
+  if #ToInterrupt > 0 and WReady and menu.interrupt then
+    for _, ability in pairs(ToInterrupt) do
+      if spell.name == ability and unit.team ~= myHero.team and GetDistance(unit) < Spell.W.Range then
+        CastSpell(_W, unit.x, unit.z)
+      end
+    end
   end
-end 
+end
 
 function getMousePos(range)
   local temprange = range or SkillWard.range
@@ -254,6 +284,5 @@ end
 function OnDraw()
   if menu.drawab.drawq then DrawCircle(player.x, player.y, player.z, Spell.Q.Range, 0xffff0000) end
   if menu.drawab.draww then DrawCircle(player.x, player.y, player.z, Spell.W.Range, 0xffff0000) end
-  if menu.drawab.drawe then DrawCircle(player.x, player.y, player.z, Spell.E.Range, 0xffff0000) end
   if menu.drawab.drawr then DrawCircle(player.x, player.y, player.z, Spell.R.Range, 0xffff0000) end
 end
