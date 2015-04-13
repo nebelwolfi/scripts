7d681451e13918594764b8129930e6b5071ac835
nebelwolfi <dr.backdooring@gmail.com>
2015-04-13 13:27:53 +0200

    hue

--- src/Lulu.lua
+++ src/Lulu.lua
@@ -1,6 +1,6 @@
 if myHero.charName ~= "Lulu" then return end
 
-local version = 0.17 -- REMEMBER: UPDATE .version FILE ASWELL FOR IN-GAME PUSH!
+local version = 0.18 -- REMEMBER: UPDATE .version FILE ASWELL FOR IN-GAME PUSH!
 local AUTO_UPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/nebelwolfi/scripts/master/src/Lulu.lua".."?rand="..math.random(1,10000)
@@ -64,8 +64,8 @@ local InterruptList = {
     { charName = "Warwick", spellName = "InfiniteDuress"}
 }
 local ToInterrupt = {}
-EnemyMinions = minionManager(MINION_ENEMY, Spell.Q.Range, myHero, MINION_SORT_MAXHEALTH_DEC)
-JungleMinions = minionManager(MINION_JUNGLE, Spell.Q.Range, myHero, MINION_SORT_MAXHEALTH_DEC)
+local enemyMinions = minionManager(MINION_ENEMY, 1500, myHero, MINION_SORT_MAXHEALTH_DEC)
+local jungleMinions = minionManager(MINION_JUNGLE, 1500, myHero, MINION_SORT_MAXHEALTH_DEC)
 
 --A basic BoL template for the Eclipse Lua Development Kit module's execution environment written by Nader Sl.
 player = GetMyHero()
@@ -95,10 +95,10 @@ function LoadMenu()
     
     menu:addSubMenu("AutoR Settings", "growthsettings")
     menu.growthsettings:addParam("user","Auto use R", SCRIPT_PARAM_ONOFF, true)
-    menu.growthsettings:addParam("autorper", "Auto R ally under health %", SCRIPT_PARAM_SLICE, 30, 1, 100, 0)
+    --menu.growthsettings:addParam("autorper", "Auto R ally under health %", SCRIPT_PARAM_SLICE, 30, 1, 100, 0) DEPRECATED
     menu.growthsettings:addParam("smartr","Use if x enemies hitable", SCRIPT_PARAM_SLICE, 3, 1, 5, 0)
     
-    menu.growthsettings:addSubMenu("Save Priority","custompriority")
+    menu.growthsettings:addSubMenu("Priority","custompriority")
     Boot()
     
     menu:addSubMenu("Harass Settings", "harass")
@@ -121,7 +121,9 @@ function LoadMenu()
     menu:addParam("combokey", "Combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
     menu:addParam("harasskey", "Hold for Harass", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
     menu:addParam("harasstoggle", "Toggle for Harass", SCRIPT_PARAM_ONKEYTOGGLE, false, string.byte("T "))
-    
+    menu:permaShow("combokey")
+    menu:permaShow("harasskey")
+    menu:permaShow("harasstoggle")
     menu:addParam("info2", " >> Version ", SCRIPT_PARAM_INFO, version)
 end
 
@@ -136,7 +138,7 @@ function Boot()
       enemyHerosCount = enemyCount
     end
   end
-  menu.growthsettings.custompriority:addParam("info1"," >> Highest priority to save: ", SCRIPT_PARAM_INFO, 1)
+  menu.growthsettings.custompriority:addParam("info1"," >> Highest priority to R: ", SCRIPT_PARAM_INFO, 5)
   alliedHeros = {}
   alliedHerosCount = 0
     for i = 1, heroManager.iCount do
@@ -144,8 +146,8 @@ function Boot()
     if hero.team == player.team then
       local alliedCount = alliedHerosCount + 1
       alliedHeros[alliedCount] = {object = hero, q = 0, w = 0, e = 0, r = 0, dfg = 0, ig = 0, myDamage = 0, manaCombo = 0}
+      menu.growthsettings.custompriority:addParam("champ"..alliedHerosCount,hero.charName,SCRIPT_PARAM_SLICE, 1, 1, 5, 0)
       alliedHerosCount = alliedCount
-      menu.growthsettings.custompriority:addParam("champ"..(alliedHerosCount-1),hero.charName,SCRIPT_PARAM_SLICE, 1, 1, 5, 0)
     end
   end
   BootDone = true
@@ -163,6 +165,8 @@ end
 -- handles script logic, a pure high speed loop
 function OnTick()
   if myHero.dead or not BootDone then return end
+  tick=GetTickCount()
+  CalcAllyDamage()
   Check()
   if menu.combokey then
       Combo()
@@ -190,14 +194,6 @@ end
 
 function SmartR()
   if RReady and menu.growthsettings.user then
-    if player.health < (player.maxHealth*(menu.growthsettings.autorper*0.01)) and countEnemy(player, 200) >= 1 then
-      CastSpell(_R, player)
-    end
-    for _, i in pairs (allies) do
-      if i.health < (i.maxHealth*(menu.growthsettings.autorper*0.01)) and countEnemy(i, 200) >= 1 then
-        CastSpell(_R, i)
-      end
-    end
     if countEnemy(player, 200) >= menu.growthsettings.smartr then
       CastSpell(_R, player)
     end
@@ -279,6 +275,85 @@ function getMousePos(range)
   local MyPos = Vector(myHero.x, myHero.y, myHero.z)
   local MousePos = Vector(mousePos.x, mousePos.y, mousePos.z)
   return MyPos - (MyPos - MousePos):normalized() * SkillWard.range
+end
+
+local OldHP = {0,0,0,0,0,0,0,0,0,0}
+local ChampDMG = {0,0,0,0,0,0,0,0,0,0}
+local lastDMGtick = {0,0,0,0,0,0,0,0,0,0}
+local tick=0
+function CalcAllyDamage()
+  for i = 1, heroManager.iCount, 1 do
+    local target = heroManager:getHero(i)
+    if (target.team == myHero.team) then
+      if (target.health >= OldHP[i]) then
+        if (tick-lastDMGtick[i] > 1000) then ChampDMG[i] = 0 end
+      else
+        lastDMGtick[i]=tick
+        ChampDMG[i] = ChampDMG[i] + (OldHP[i] - target.health)
+        local DamageLvlTaken=math.round(DamageImpactTaken(i)*10)
+        if (DamageLvlTaken>=1) then
+          if (Plugin_FocusedAlly) then
+            Plugin_FocusedAlly(target,DamageLvlTaken)
+          end
+        end
+      end
+      OldHP[i] = target.health
+    end
+  end
+end
+
+function DamageImpactTaken(iHero)
+  local target = heroManager:getHero(iHero)
+  if (target.health==0) then return 0 end
+  local x=(ChampDMG[iHero]/target.health)
+  if x > 9 then 
+    return 10
+  else
+    return x
+  end
+end
+
+function Plugin_FocusedAlly(target,DamageLvlTaken)
+  local targetP = 0
+  for i = 1, 5 do
+    local hero = allies[i]
+      if target == myHero then 
+        targetP = menu.growthsettings.custompriority.champ1
+        return
+      elseif hero == target then
+        if i == 1 then
+          targetP = menu.growthsettings.custompriority.champ1
+        elseif i == 2 then
+          targetP = menu.growthsettings.custompriority.champ2
+        elseif i == 3 then
+          targetP = menu.growthsettings.custompriority.champ3
+        elseif i == 4 then
+          targetP = menu.growthsettings.custompriority.champ4
+        elseif i == 5 then
+          targetP = menu.growthsettings.custompriority.champ5
+        end
+    end
+  end
+  PrintChat("Dmglvl: "..DamageLvlTaken..", Champ: "..target.charName..", Priority: "..targetP)
+  if DamageLvlTaken>=30 and (myHero:CanUseSpell(_R) == READY) and (GetDistance(target) < 900 ) then
+    if targetP > 4 and DamageLvlTaken>=30 then
+      CastSpell(_R, target)
+    elseif targetP > 3 and DamageLvlTaken>=40 then
+      CastSpell(_R, target)
+    elseif targetP > 2 and DamageLvlTaken>=55 then
+      CastSpell(_R, target)
+    elseif targetP > 1 and DamageLvlTaken>=70 then
+      CastSpell(_R, target)
+    elseif targetP > 0 and DamageLvlTaken>=85 then
+      CastSpell(_R, target)
+    end
+  elseif DamageLvlTaken>=4 and (myHero:CanUseSpell(_E) == READY) and (GetDistance(target) < 650 ) then
+    if target == myHero then
+      CastSpell(_E, target)
+    elseif DamageLvlTaken>6 then
+      CastSpell(_E, target)
+    end
+  end
 end
 
 function OnDraw()
