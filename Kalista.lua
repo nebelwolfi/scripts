562081b652d6a7d10feaf38845284ddcfee15c97
Inspired-gos <drbackdooring@gmail.com>
2015-07-17 20:44:40 +0200

    Update Kalista.lua

--- Kalista.lua
+++ Kalista.lua
@@ -7,7 +7,6 @@
     myHero = myHer0
     myHeroPos = GetOrigin(myHero)
     if (GetObjectType(unit) == Obj_AI_Hero or GetObjectType(unit) == Obj_AI_Minion or GetObjectType(unit) == Obj_AI_Camp) and ValidTarget(unit, 1000) then
-      MessageBox(0,"hi","hi",0)
       local TotalDmg = GetBonusDmg(myHero)+GetBaseDamage(myHero)
       local dmgE = (GotBuff(unit,"kalistaexpungemarker") > 0 and (10 + (10 * GetCastLevel(myHero,_E)) + (TotalDmg * 0.6)) + (GotBuff(unit,"kalistaexpungemarker")-1) * (kalE(GetCastLevel(myHero,_E)) + (0.15 + 0.03 * GetCastLevel(myHero,_E))*TotalDmg) or 0)
       local dmg = CalcDamage(unit, dmgE, "AD")
