37cd09ab9d18e4a4c7f4ecf0b1c9c7a7a22c1082
Inspired-gos <drbackdooring@gmail.com>
2015-07-17 20:40:43 +0200

    Update Kalista.lua

--- Kalista.lua
+++ Kalista.lua
@@ -5,13 +5,15 @@
 
   function ObjectLoopEvent(unit, myHero)
     if (GetObjectType(unit) == Obj_AI_Hero or GetObjectType(unit) == Obj_AI_Minion or GetObjectType(unit) == Obj_AI_Camp) and ValidTarget(unit, 1000) then
+      MessageBox(0,"hi","hi",0)
       local TotalDmg = GetBonusDmg(myHero)+GetBaseDamage(myHero)
       local dmgE = (GotBuff(unit,"kalistaexpungemarker") > 0 and (10 + (10 * GetCastLevel(myHero,_E)) + (TotalDmg * 0.6)) + (GotBuff(unit,"kalistaexpungemarker")-1) * (kalE(GetCastLevel(myHero,_E)) + (0.15 + 0.03 * GetCastLevel(myHero,_E))*TotalDmg) or 0)
       local dmg = CalcDamage(unit, dmgE, "AD")
       local hp = GetCurrentHP(unit)
       local targetPos = GetOrigin(unit)
       local drawPos = WorldToScreen(1,targetPos.x,targetPos.y,targetPos.z)
-      if dmg > 0 then DrawText(math.floor(dmg/hp*100).."%",20,drawPos.x,drawPos.y,0xffffffff)
+      if dmg > 0 then 
+        DrawText(math.floor(dmg/hp*100).."%",20,drawPos.x,drawPos.y,0xffffffff)
         if hp > 0 and dmg >= hp then CastTargetSpell(unit, _E) end
       end
     end
