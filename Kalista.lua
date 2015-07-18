b33868dc4f3ce8202cebe491b2ce9e402c83aefd
Inspired-gos <drbackdooring@gmail.com>
2015-07-18 18:46:09 +0200

    Update Kalista.lua

--- Kalista.lua
+++ Kalista.lua
@@ -1,11 +1,4 @@
-  local version = 5
-  local myHero = nil
-  local myHeroPos = nil
-  local walk = false
-
-  function AfterObjectLoopEvent(myHer0)
-    myHero = myHer0
-    myHeroPos = GetOrigin(myHero)
+function AfterObjectLoopEvent(myHero)
     local movePos = GenerateMovePos()
     local unit = GetCurrentTarget()
     if not KeyIsDown(0x20) then return end
@@ -36,46 +29,12 @@
     else
       MoveToXYZ(movePos.x, movePos.y, movePos.z)
     end
-  end
+end
 
-  function kalE(x)
-    if x <= 1 then 
-      return 10
-    else 
-      return kalE(x-1) + 2 + x
-    end 
-  end
-
-  function ValidTarget(unit, range)
-    range = range or math.huge
-    if unit == nil or GetOrigin(unit) == nil or IsDead(unit) or GetTeam(unit) == GetTeam(myHero) or GetDistance(GetOrigin(unit)) > range*range then return false end
-    return true
-  end
-
-  function GetDistance(p1,p2)
-    p2 = p2 or myHeroPos
-    local dx = p1.x - p2.x
-    local dz = (p1.z or p1.y) - (p2.z or p2.y)
-    return dx*dx + dz*dz
-  end
-
-  function CalcDamage(source, target, addmg, apdmg)
-    local ADDmg             = addmg or 0
-    local APDmg             = apdmg or 0
-    local ArmorPen          = math.floor(GetArmorPenFlat(source))
-    local ArmorPenPercent   = math.floor(GetArmorPenPercent(source)*100)/100
-    local Armor             = GetArmor(target)*ArmorPenPercent-ArmorPen
-    local ArmorPercent      = Armor > 0 and math.floor(Armor*100/(100+Armor))/100 or math.ceil(Armor*100/(100-Armor))/100
-    local MagicPen          = math.floor(GetMagicPenFlat(source))
-    local MagicPenPercent   = math.floor(GetMagicPenPercent(source)*100)/100
-    local MagicArmor        = GetMagicResist(target)*MagicPenPercent-MagicPen
-    local MagicArmorPercent = MagicArmor > 0 and math.floor(MagicArmor*100/(100+MagicArmor))/100 or math.ceil(MagicArmor*100/(100-MagicArmor))/100
-    return math.floor(ADDmg*(1-ArmorPercent))+math.floor(APDmg*(1-MagicArmorPercent))
-  end
-
-  function GenerateMovePos()
-    local mPos = GetMousePos()
-    local tV = {x = (mPos.x-myHeroPos.x), z = (mPos.z-myHeroPos.z)}
-    local len = math.sqrt(tV.x * tV.x + tV.z * tV.z)
-    return {x = myHeroPos.x + 250 * tV.x / len, y = 0, z = myHeroPos.z + 250 * tV.z / len}
-  end
+function kalE(x)
+	if x <= 1 then 
+		return 10
+	else 
+		return kalE(x-1) + 2 + x
+	end 
+end
