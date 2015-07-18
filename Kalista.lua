23e74775c5333a509dde5caafea30ef582dd92d0
Inspired-gos <drbackdooring@gmail.com>
2015-07-19 01:29:57 +0200

    Update Kalista.lua

--- Kalista.lua
+++ Kalista.lua
@@ -2,9 +2,9 @@ function AfterObjectLoopEvent(myHero)
     local movePos = GenerateMovePos()
     local unit = GetTarget(GetRange(myHero))
     if not KeyIsDown(0x20) then return end
-    if ValidTarget(unit, 1000) then
+    if ValidTarget(unit, 1200) then
       local TotalDmg = GetBonusDmg(myHero)+GetBaseDamage(myHero)
-      local dmgE = (GotBuff(unit,"kalistaexpungemarker") > 0 and (10 + (10 * GetCastLevel(myHero,_E)) + (TotalDmg * 0.6)) + (GotBuff(unit,"kalistaexpungemarker")-1) * (kalE(GetCastLevel(myHero,_E)) + (0.15 + 0.03 * GetCastLevel(myHero,_E))*TotalDmg) or 0)
+      local dmgE = (GotBuff(unit,"kalistaexpungemarker") > 0 and (10 + (10 * GetCastLevel(myHero,_E)) + (TotalDmg * 0.6)) + (GotBuff(unit,"kalistaexpungemarker")-1) * (kalE(GetCastLevel(myHero,_E)) + (0.175 + 0.025 * GetCastLevel(myHero,_E))*TotalDmg) or 0)
       local dmg = CalcDamage(myHero, unit, dmgE)
       local hp = GetCurrentHP(unit)
       local targetPos = GetOrigin(unit)
@@ -14,11 +14,13 @@ function AfterObjectLoopEvent(myHero)
         if hp > 0 and dmg >= hp then CastTargetSpell(unit, _E) end
       end
     end
-    if ValidTarget(unit, 1150) then
+    if ValidTarget(unit, 1175) then
       local QPred = GetPredictionForPlayer(myHeroPos,unit,GetMoveSpeed(unit),1750,250,1150,70,true,true)
       if walk then
         walk = false
-        MoveToXYZ(movePos.x, movePos.y, movePos.z)
+        if GetDistance(GetMousePos()) > GetHitBox(myHero) then
+          MoveToXYZ(movePos.x, 0, movePos.z)
+        end
       elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
         walk = true
         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
@@ -29,14 +31,10 @@ function AfterObjectLoopEvent(myHero)
         end
       end
     else
-      MoveToXYZ(movePos.x, movePos.y, movePos.z)
+      if GetDistance(GetMousePos()) > GetHitBox(myHero) then
+        MoveToXYZ(movePos.x, 0, movePos.z)
+      end
     end
 end
 
-function kalE(x)
-	if x <= 1 then 
-		return 10
-	else 
-		return kalE(x-1) + 2 + x
-	end 
-end
+function kalE(x) if x <= 1 then return 10 else return kalE(x-1) + 2 + x end end
