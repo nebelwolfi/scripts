7138727cc75d3fd5c50bbb8cb0dc6c1de1b9f8f5
Inspired-gos <drbackdooring@gmail.com>
2015-07-18 22:35:52 +0200

    Create Orianna.lua

---
+++ Orianna.lua
@@ -0,0 +1,43 @@
+ballPos = nil
+ballDel = 0
+
+function AfterObjectLoopEvent(myHero)
+	if GetBall() then
+		ballPos = GetOrigin(GetBall())
+	end
+	if ballPos and ballDel + 1750 < GetTickCount() then
+		DrawCircle(ballPos.x,ballPos.y,ballPos.z,150,5,150,0xff00ff00)
+	end
+	if not KeyIsDown(0x20) then return end
+	local unit = GetTarget(1000)
+	if ValidTarget(unit, 1000) then
+		local uPos = GetOrigin(unit)
+		local QPred = GetPredictionForPlayer(GetMyHeroPos(),unit,GetMoveSpeed(unit),1200,250,825,175,true,true)
+		if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
+			local tP = GenerateThrowPos(QPred.PredPos)
+    		CastSkillShot(_Q,tP.x,tP.y,tP.z)
+		end
+		if CanUseSpell(myHero, _W) == READY and (ballPos and GetDistanceSqr(ballPos,GetOrigin(unit)) < 225*225 or GetDistanceSqr(GetMyHeroPos(),GetOrigin(unit)) < 225*225) then
+    		CastTargetSpell(myHero, _W)
+		end
+		if CanUseSpell(myHero, _E) == READY and ballPos and GetDistanceSqr(ballPos,GetOrigin(unit)) < 80*80 and GetDistanceSqr(GetMyHeroPos(),GetOrigin(unit)) < GetDistanceSqr(GetMyHeroPos(),ballPos) then
+    		CastTargetSpell(myHero, _E)
+		end
+	end
+end
+
+function OnProcessSpell(unit, spell)
+	if unit and unit == GetMyHero() and spell and spell.name == "OrianaIzunaCommand" then
+		ballPos = nil
+	end
+	if unit and unit == GetMyHero() and spell and spell.name == "OrianaRedactCommand" then
+		ballPos = spell.target
+		ballDel = GetTickCount()
+	end
+end
+
+function GenerateThrowPos(pos)
+    local tV = {x = (pos.x-GetMyHeroPos().x), z = (pos.z-GetMyHeroPos().z)}
+    local len = math.sqrt(tV.x * tV.x + tV.z * tV.z)
+    return {x = pos.x + 55 * tV.x / len, y = 0, z = pos.z + 55 * tV.z / len}
+end
