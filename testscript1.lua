355ea7acbf857f0aa292311bdbf9c21a0660773e
Inspired-gos <drbackdooring@gmail.com>
2015-07-18 19:47:04 +0200

    Update testscript1.lua

--- testscript1.lua
+++ testscript1.lua
@@ -1,15 +1,2 @@
-local reloading = 0
-
-function AfterObjectLoopEvent(myHero)
-	if KeyIsDown(0x78) then
-		reloading = 1
-	else
-		if reloading == 1 then
-			package.loaded[ 'Inspired' ] = nil
-			package.loaded[ GetObjectName(myHero) ] = nil
-			require('Inspired')
-			require(GetObjectName(myHero))
-			reloading = 0
-		end
-	end
-end
+require('Inspired')
+require(GetObjectName(GetMyHero()))
