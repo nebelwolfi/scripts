e0963bd8c047e7f9be68f2612a8cfeb87d858825
nebelwolfi <dr.backdooring@gmail.com>
2015-07-19 03:26:38 +0200

    asdf

--- Common/Cassiopeia.lua
+++ Common/Cassiopeia.lua
@@ -12,15 +12,15 @@ function AfterObjectLoopEvent(myHero)
 		local WPred = GetPredictionForPlayer(GetMyHeroPos(),unit,GetMoveSpeed(unit),2500,250,925,90,true,true)
 		-- is e ready? is unit in distance? is unit poisoned?
 		if CanUseSpell(myHero, _E) == READY and IsInDistance(unit, 700) and (GotBuff(unit,"cassiopeiamiasmapoison") > 0 or GotBuff(unit,"cassiopeianoxiousblastpoison") > 0) then
-			-- cast e!
+			-- cast e targeted!
 			CastTargetSpell(unit, _E)
 		-- is w ready? is unit in distance? is hitchance high enough?
 		elseif CanUseSpell(myHero, _W) == READY and IsInDistance(unit, 925) and WPred.HitChance == 1 then
-			-- cast w!
+			-- cast w towards position where enemy will be!
 			CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
 		-- is q ready? is unit in distance? is hitchance high enough?
 		elseif CanUseSpell(myHero, _Q) == READY and IsInDistance(unit, 850) and QPred.HitChance == 1 then
-			-- cast q!
+			-- cast q towards position where enemy will be!
 			CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 		end
 	end
