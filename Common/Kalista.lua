fdba121b2739613af63588c1c892bc3cac32c01e
nebelwolfi <dr.backdooring@gmail.com>
2015-07-19 03:44:37 +0200

    asdf

--- Common/Kalista.lua
+++ Common/Kalista.lua
@@ -1,26 +1,51 @@
+-- this gets executed every frame
 function AfterObjectLoopEvent(myHero)
-    local movePos = GenerateMovePos()
-    local unit = GetTarget(GetRange(myHero))
-    if not KeyIsDown(0x20) then return end
+  -- if we dont press spacebar we do nothing
+  if not KeyIsDown(0x20) then return end
+  -- iterate through all enemy heroes
+  for k,unit in pairs(GetEnemyHeroes()) do
+    -- is the current unit is a valid target
     if ValidTarget(unit, 1200) then
+      -- our total ad
       local TotalDmg = GetBonusDmg(myHero)+GetBaseDamage(myHero)
+      -- out e damage, taken stack count into calc
       local dmgE = (GotBuff(unit,"kalistaexpungemarker") > 0 and (10 + (10 * GetCastLevel(myHero,_E)) + (TotalDmg * 0.6)) + (GotBuff(unit,"kalistaexpungemarker")-1) * (kalE(GetCastLevel(myHero,_E)) + (0.175 + 0.025 * GetCastLevel(myHero,_E))*TotalDmg) or 0)
+      -- calculates damage on enemy, with armor taken into calc
       local dmg = CalcDamage(myHero, unit, dmgE)
+      -- hp of target
       local hp = GetCurrentHP(unit)
+      -- get target position
       local targetPos = GetOrigin(unit)
+      -- make draw position
       local drawPos = WorldToScreen(1,targetPos.x,targetPos.y,targetPos.z)
+      -- if we do some dmg
       if dmg > 0 then 
+        -- draw percentage of dmg
         DrawText(math.floor(dmg/hp*100).."%",20,drawPos.x,drawPos.y,0xffffffff)
-        if hp > 0 and dmg >= hp then CastTargetSpell(unit, _E) end
+        -- if our dmg is greater than target hp
+        if hp > 0 and dmg >= hp then 
+          -- cast e
+          CastTargetSpell(unit, _E) 
+        end
       end
     end
-    if ValidTarget(unit, 1175) then
-      local QPred = GetPredictionForPlayer(myHeroPos,unit,GetMoveSpeed(unit),1750,250,1150,70,true,true)
-      if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
-        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-      end
+  end
+  -- grab best target in 1175 range
+  local unit = GetTarget(1175)
+  -- if the target is valid and (still) in 1175 range
+  if ValidTarget(unit, 1175) then
+    -- following line is used to predict enemy position
+    -- GetPredictionForPlayer(startPosition, targetUnit, targetUnitMoveSpeed, spellTravelSpeed, spellDelay, spellRange, spellWidth, collision, addHitBox)
+    local QPred = GetPredictionForPlayer(myHeroPos,unit,GetMoveSpeed(unit),1750,250,1150,70,true,true)
+    -- is q ready? is hitchance high enough?
+    if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
+      -- cast q towards position where enemy will be!
+      CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
     end
-    IWalk()
+  end
+  -- walk and autoattack
+  IWalk()
 end
 
+-- recursively calculates stackdmg
 function kalE(x) if x <= 1 then return 10 else return kalE(x-1) + 2 + x end end
