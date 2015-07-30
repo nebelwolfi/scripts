98c925d13187eeb0dd735820d996d2da16dde328
nebelwolfi <dr.backdooring@gmail.com>
2015-07-30 06:18:41 +0200

    asdf

--- Common/IWalk.lua
+++ Common/IWalk.lua
@@ -11,55 +11,9 @@ waitTickCount = 0
 DelayAction(function() MakeMenu() end, 0)
 
 OnLoop(function()
-  if IWalkConfig.D then DmgCalc() end
   if waitTickCount > GetTickCount() then return end
   IWalk()
 end)
-
-function DmgCalc()
-  for _,unit in pairs(GetEnemyHeroes()) do
-    if ValidTarget(unit) then
-      local hPos = GetHPBarPos(unit)
-      DrawText(PossibleDmg(unit), 15, hPos.x, hPos.y+20, 0xffffffff)
-    end
-  end
-end
-
-function PossibleDmg(unit)
-  local addDamage = GetBonusDmg(myHero)
-  local TotalDmg = (GetBonusDmg(myHero)+GetBaseDamage(myHero))*(((IWalkConfig.R and (GetCastName(myHero, _R) ~= "RivenFengShuiEngine" or CanUseSpell(myHero, _R)))) and 1.2 or 1)
-  local dmg = 0
-  local cthp = GetCurrentHP(unit)
-  local mthp = GetMaxHP(unit)
-  if GetObjectName(myHero) == "Riven" then
-    local dmg = 0
-    local mlevel = GetLevel(myHero)
-    local pdmg = CalcDamage(myHero, unit, 5+math.max(5*math.floor((mlevel+2)/3)+10,10*math.floor((mlevel+2)/3)-15)*TotalDmg/100)
-    if CanUseSpell(myHero, _Q) == READY then
-      local level = GetCastLevel(myHero, _Q)
-      dmg = dmg + CalcDamage(myHero, unit, 20*level+(0.35+0.05*level)*TotalDmg-10)*3+CalcDamage(myHero, unit, TotalDmg)*3+pdmg*3
-    end
-    if CanUseSpell(myHero, _W) == READY then
-      local level = GetCastLevel(myHero, _W)
-      dmg = dmg + CalcDamage(myHero, unit, 20+30*level+TotalDmg)+CalcDamage(myHero, unit, TotalDmg)+pdmg
-    end
-    if (CanUseSpell(myHero, _R) == READY or GetCastName(myHero, _R) ~= "RivenFengShuiEngine") and IWalkConfig.R then
-      local level = GetCastLevel(myHero, _R)
-      local rdmg = CalcDamage(myHero, unit, (40+40*level+0.6*addDamage)*(math.min(3,math.max(1,4*(mthp-cthp)/mthp))))
-      if rdmg > cthp and ValidTarget(unit, 800) and GetCastName(myHero, _R) ~= "RivenFengShuiEngine" and IWalkConfig.Combo then 
-        local unitPos = GetOrigin(unit)
-        CastSkillShot(_R, unitPos.x, unitPos.y, unitPos.z) 
-      end
-      cthp = cthp - dmg
-      rdmg = CalcDamage(myHero, unit, (40+40*level+0.6*addDamage)*(math.min(3,math.max(1,4*(mthp-cthp)/mthp))))
-      dmg = dmg + rdmg
-    end
-    return dmg > cthp and "Killable" or math.floor(100*dmg/cthp).."% Dmg"
-  else
-    dmg = CalcDamage(myHero, unit, TotalDmg)
-    return math.ceil(cthp/dmg).." AA"
-  end
-end
 
 function IWalk()
   if IWalkConfig.LastHit or IWalkConfig.LaneClear or IWalkConfig.Harass then
@@ -101,14 +55,6 @@ function DoWalk()
       if gapcloserTable[GetObjectName(myHero)] and ValidTarget(unit, myRange + 250) and IWalkConfig[str[gapcloserTable[GetObjectName(myHero)]].."g"] and CanUseSpell(myHero, gapcloserTable[GetObjectName(myHero)]) == READY then
         local unitPos = GetOrigin(unit)
         CastSkillShot(gapcloserTable[GetObjectName(myHero)], unitPos.x, unitPos.y, unitPos.z)
-        if GetObjectName(myHero) == "Riven" and IWalkConfig["W"] and CanUseSpell(myHero, _W) == READY then
-          if PossibleDmg(unit):find("Killable") and IWalkConfig.R then
-            DelayAction(function() CastTargetSpell(myHero, _R) end, 137)
-          else
-            DelayAction(function() CastTargetSpell(myHero, _W) end, 137)
-          end
-          orbTable.lastAA = 0
-        end
       else
         Move()
       end
@@ -165,10 +111,6 @@ function WindUp(unit)
       if CanUseSpell(myHero, k) == READY and IWalkConfig[str[k]] and GetDistanceSqr(GetOrigin(unit)) < myRange * myRange and (not GetObjectName(myHero) == "Quinn" or CanUseSpell(myHero, _E) ~= READY) then
         local unitPos = GetOrigin(unit)
         CastSkillShot(k, unitPos.x, unitPos.y, unitPos.z)
-        if GetObjectName(myHero) == "Riven" then
-          local unitPos = GetOrigin(unit)
-          MoveToXYZ(unitPos.x, unitPos.y, unitPos.z)
-        end
         orbTable.lastAA = 0
         return true
       end
@@ -177,10 +119,8 @@ function WindUp(unit)
   if aaResetTable3[GetObjectName(myHero)] then
     for _,k in pairs(aaResetTable3[GetObjectName(myHero)]) do
       if CanUseSpell(myHero, k) == READY and IWalkConfig[str[k]] and GetDistanceSqr(GetOrigin(unit)) < myRange * myRange then
-        if GetObjectName(myHero) ~= "Quinn" or GotBuff(unit, "QuinnW") < 1 then
-          orbTable.lastAA = 0
-          CastTargetSpell(unit, k)
-        end
+        orbTable.lastAA = 0
+        CastTargetSpell(unit, k)
         return true
       end
     end
@@ -215,36 +155,12 @@ function MakeMenu()
     k = gapcloserTable[GetObjectName(myHero)]
     IWalkConfig.addParam(str[k].."g", "Gapclose with "..str[k], SCRIPT_PARAM_ONOFF, true)
   end
-  DoChampionPluginMenu()
   IWalkConfig.addParam("I", "Cast Items", SCRIPT_PARAM_ONOFF, true)
   IWalkConfig.addParam("S", "Skillfarm", SCRIPT_PARAM_ONOFF, true)
-  IWalkConfig.addParam("D", "Damage Calc", SCRIPT_PARAM_ONOFF, true)
   IWalkConfig.addParam("C", "AA Range Circle", SCRIPT_PARAM_ONOFF, true)
 
   IWalkConfig.addParam("LastHit", "LastHit", SCRIPT_PARAM_KEYDOWN, string.byte("X"))
   IWalkConfig.addParam("Harass", "Harass", SCRIPT_PARAM_KEYDOWN, string.byte("C"))
   IWalkConfig.addParam("LaneClear", "LaneClear", SCRIPT_PARAM_KEYDOWN, string.byte("V"))
   IWalkConfig.addParam("Combo", "Combo", SCRIPT_PARAM_KEYDOWN, string.byte(" "))
-end
-
-function DoChampionPluginMenu()
-  if GetObjectName(myHero) == "Ashe" then 
-    IWalkConfig.addParam("Q", "Use Q at 5 stacks", SCRIPT_PARAM_ONOFF, true) 
-    IWalkConfig.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true) 
-  end
-  if GetObjectName(myHero) == "Riven" then 
-    IWalkConfig.addParam("R", "Use R if Kill", SCRIPT_PARAM_ONOFF, true) 
-  end
-end
-
-function DoChampionPlugins(unit)
-  if GetObjectName(myHero) == "Ashe" then
-    if GotBuff(myHero, "asheqcastready") > 0 and IWalkConfig.Q then
-      CastSpell(_Q)
-    end
-    if CanUseSpell(myHero, _W) == READY and IWalkConfig.W then
-      local unitPos = GetOrigin(unit)
-      CastSkillShot(_W, unitPos.x, unitPos.y, unitPos.z)
-    end
-  end
 end
