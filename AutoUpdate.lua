72344e0840e8b4eb52f2eaf8f2a24ccc2eb43577
Inspired-gos <drbackdooring@gmail.com>
2015-11-15 16:11:18 +0100

    asdf

--- AutoUpdate.lua
+++ AutoUpdate.lua
@@ -1,1 +1,6 @@
-print("Hello World!")
+require("Inspired")
+local WebLuaFile = "/Inspired-gos/scripts/master/AutoUpdate.lua"
+local WebVersion = "/Inspired-gos/scripts/master/AutoUpdate.version"
+local LocalLua = "AutoUpdate.lua"
+local LocalVersion = 1
+AutoUpdate(WebLuaFile,WebVersion,LocalLua,LocalVersion)
