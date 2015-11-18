5ffdd3be062978554c50c1f3eb878dda6a5d0ceb
Inspired-gos <drbackdooring@gmail.com>
2015-11-18 14:46:48 +0100

    asdf

--- Common/IPrediction.lua
+++ Common/IPrediction.lua
@@ -48,8 +48,8 @@ do
 		BUFF_DISARM = 31,
 		spells = {
 			{name = "katarinar", duration = 1},
-			{name = "drain", duration = 1},
-			{name = "crowstorm", duration = 1},
+			{name = "drain", duration = 5},
+			{name = "crowstorm", duration = 1.5},
 			{name = "consume", duration = 1.5},
 			{name = "absolutezero", duration = 1},
 			{name = "rocketgrab", duration = 0.5},
@@ -70,40 +70,6 @@ do
 			{name = "gate", duration = 1.5},
 		},
 		Spells = {},
-		dashes = {
-			{name = "ahritumble", duration = 0.25, range = 450},
-			{name = "akalishadowdance", duration = 0.25, range = 700},
-			{name = "headbutt", duration = 0.25, range = 650},
-			{name = "caitlynentrapment", duration = 0.25, range = 0},
-			{name = "carpetbomb", duration = 0.25, range = 800},
-			{name = "dianateleport", duration = 0.25, range = 825},
-			{name = "fizzpiercingstrike", duration = 0.25, range = 550, max = true},
-			{name = "gragasbodyslam", duration = 0.25, range = 600},
-			{name = "gravesmove", duration = 0.25, range = 425},
-			{name = "ireliagatotsu", duration = 0.25, range = 650},
-			{name = "jarvanivdragonstrike", duration = 0.25, range = 770},
-			{name = "jaxleapstrike", duration = 0.25, range = 700},
-			{name = "khazixe", duration = 0.25, range = 600},
-			{name = "leblancslide", duration = 0.25, range = 600},
-			{name = "leblancslidem", duration = 0.25, range = 600},
-			{name = "blindmonkqtwo", duration = 0.25, endP = true},
-			{name = "blindmonkwone", duration = 0.25, endP = true},
-			{name = "luciane", duration = 0.25, range = 445, max = true},
-			{name = "nocturneparanoia2", duration = 0.25, endP = true},
-			{name = "pantheon_leapbash", duration = 0.25, endP = true},
-			{name = "renektonsliceanddice", duration = 0.25, range = 450, max = true},
-			{name = "riventricleave", duration = 0.25, range = 275, max = true},
-			{name = "rivenfeint", duration = 0.25, range = 350, max = true},
-			{name = "sejuaniarcticassault", duration = 0.25, range = 650, max = true},
-			{name = "shenshadowdash", duration = 0.25, range = 600, max = true},
-			{name = "shyvanatransformcast", duration = 0.25, range = 1000, max = true},
-			{name = "rocketjump", duration = 0.25, range = 900},
-			{name = "slashcast", duration = 0.25, range = 650},
-			{name = "vaynetumble", duration = 0.25, range = 300},
-			{name = "monkeykingnimbus", duration = 0.25, endP = true},
-			{name = "xenzhaosweep", duration = 0.25, endP = true},
-			{name = "yasuodashwrapper", duration = 0.25, range = 475, max = true},
-		},
 		blinks = {
 			{name = "ezrealarcaneshift", range = 475, delay = 0.25, delay2=0.8},
 			{name = "deceive", range = 400, delay = 0.25, delay2=0.8},
@@ -147,8 +113,8 @@ do
 					end
 				end
 			end
-			Prediction.Core.RemoveOldWayPoints(unit)
-			if GetTeam(unit) ~= GetTeam(myHero) then
+			Prediction.WayPointManager.RemoveOldWayPoints(unit)
+			if ValidTarget(unit) then
 				local I = #Prediction.Vars.DashCallbacks;
 				for i = 1, I do
 					dashCallback = Prediction.Vars.DashCallbacks[i]
@@ -160,9 +126,9 @@ do
 				local I = #Prediction.Vars.StunCallbacks;
 				for i = 1, I do
 					stunCallback = Prediction.Vars.StunCallbacks[i]
-					local x, y = Prediction.User.Interface.IsUnitStunned(unit, stunCallback.range, stunCallback.speed, stunCallback.delay, stunCallback.width, stunCallback.source)
-					if x and y <= stunCallback.width + GetHitBox(unit) then
-						stunCallback.callback(unit, y)
+					local x, y, z = Prediction.User.Interface.IsUnitStunned(unit, stunCallback.range, stunCallback.speed, stunCallback.delay, stunCallback.width, stunCallback.source)
+					if x and y <= stunCallback.width + GetHitBox(unit) and GetDistance(z) < stunCallback.range + stunCallback.width + GetHitBox(unit) then
+						stunCallback.callback(z, y)
 					end
 				end
 			end
@@ -171,14 +137,20 @@ do
 
 	function Prediction.Callback.ProcessWaypoint(unit, waypoint)
 		if unit and IsObjectAlive(unit) and GetObjectType(unit) == GetObjectType(myHero) then
-			if unit ~= myHero and waypoint.index == 1 then
+			if waypoint.dashspeed > 0 then
+				local endPos = Vector(waypoint.position)
+				local realDist = GetDistance(unit, endPos)
+				local speed = waypoint.dashspeed
+				speed = speed * (100 + waypoint.dashgravity) / 100
+				Prediction.Vars.Dashing[GetNetworkID(unit)] = {startT = GetGameTimer(), endT = GetGameTimer() + realDist/(speed), startPos = Vector(unit), endPos = endPos, dashSpeed = speed, dashDistance = realDist}
+			else
 				local current_way_point = Vector(waypoint.position)
 				local my_current_position = Vector(myHero)
 				local diff_coordinates = {x = current_way_point.x - my_current_position.x, y = current_way_point.y - my_current_position.y}
 				local polar_coordinate = {theta = math.atan2(diff_coordinates.x, diff_coordinates.y), r = Prediction.Core.pythag(diff_coordinates.x, diff_coordinates.y)}
 				Prediction.Vars.Stunned[GetNetworkID(unit)] = {}
 				if not Prediction.Vars.WayPoints[GetNetworkID(unit)] then Prediction.Vars.WayPoints[GetNetworkID(unit)] = {} end
-				table.insert(Prediction.Vars.WayPoints[GetNetworkID(unit)], {waypoints = polar_coordinate, time = GetGameTimer()})
+				table.insert(Prediction.Vars.WayPoints[GetNetworkID(unit)], {rel = polar_coordinate, time = GetGameTimer(), pos = Vector(waypoint.position), index = waypoint.index})
 			end
 		end
 	end
@@ -191,136 +163,196 @@ do
 					return
 				end
 			end
-			for _, thing in pairs(Prediction.Vars.dashes) do
-				if spell.name:lower():find(thing.name) then
-					local endPos = Vector(spell.endPos or spell.target)
-					local realDist = GetDistance(unit, endPos)
-					if not thing.endP then
-						if thing.max then
-							realDist = thing.range
-							pcall(function() endPos = Vector(spell.startPos) + Vector(Vector(spell.endPos) - Vector(spell.startPos)):normalized() * thing.range end)
-						elseif realDist > thing.range then
-							realDist = thing.range
-							pcall(function() endPos = Vector(spell.startPos) + Vector(Vector(spell.endPos) - Vector(spell.startPos)):normalized() * thing.range end)
-						end
+		end
+	end
+
+	function Prediction.Core.Collision(startP, endP, spell, type)
+		local startP = GetOrigin(startP)
+		local obj = endP
+		local endP = GetOrigin(endP)
+		local objects = {}
+		local collides = {}
+		if not type or type == Obj_AI_Minion then
+			for i=1, minionManager.maxObjects do
+				table.insert(objects, minionManager.objects[i])
+			end
+		end
+		if not type or type == Obj_AI_Hero then
+			for i=1, #heroes do
+				table.insert(objects, heroes[i])
+			end
+		end
+		for I=1, #objects do
+			local object = objects[I]
+			if object ~= obj and IsObjectAlive(object) and GetOrigin(object) ~= nil and IsVisible(object) then
+				local predP = GetOrigin(object)--Prediction.Core.Predict(object, spell, startP)
+				local ProjPoint,_,OnSegment = VectorPointProjectionOnLineSegment(startP, endP, predP)
+				if OnSegment then
+					if GetDistanceSqr(ProjPoint, predP) < (GetHitBox(object) + spell.width) ^ 2 then
+						table.insert(collides, object)
 					end
-					Prediction.Vars.Dashing[GetNetworkID(unit)] = {startT = GetGameTimer() + spell.windUpTime, endT = GetGameTimer() + spell.windUpTime + realDist/(thing.speed or 1800), startPos = Vector(spell.startPos), endPos = endPos, dashSpeed = thing.peed, dashDistance = realDist}
-					return
-				end
-			end
-			for _, thing in pairs(Prediction.Vars.blinks) do
-				if spell.name:lower() == thing.name then
-					Prediction.Vars.Stunned[GetNetworkID(unit)] = {endT = GetGameTimer() + thing.delay2, bType = Prediction.Vars.BUFF_STUN, bName = spell.name}
-					return
 				end
 			end
 		end
+		local collN = #collides
+		return collN>0, collN, collides
 	end
 
-	function Prediction.Core.RemoveOldWayPoints(unit)
-		local waypoints = Prediction.Vars.WayPoints[GetNetworkID(unit)]
-		local i = 1
-		while i <= #waypoints do
-			if (GetGameTimer() - waypoints[i].time) >= 10 then
-				table.remove(waypoints, i)
-			else
-				i = i + 1
+	function Prediction.Core.Predict(unit, spell, source)
+		local chance, pos, info = 0, nil, nil
+		if spell.collision > 0 then
+			local x, y, z = IPrediction.Collision(myHero, unit, spell)
+			if x and y-spell.collision > 0 then
+				return Prediction.User.State.WILL_COLLIDE, nil, {num = y, objects = z}
 			end
 		end
-		Prediction.Vars.WayPoints[GetNetworkID(unit)] = waypoints
+		if not unit or not IsVisible(unit) or not Prediction.WayPointManager.IsVisible(unit) then
+			return Prediction.User.State.INVISIBLE, nil
+		end
+		local i, p, d = Prediction.User.Interface.IsUnitDashing(unit, spell.range, spell.speed, spell.delay, spell.width, source)
+		if i and d <= spell.width + GetHitBox(unit) then
+			return Prediction.User.State.ENEMY_IS_DASHING, Vector(p), d
+		end
+		local i, d, p = Prediction.User.Interface.IsUnitStunned(unit, spell.range, spell.speed, spell.delay, spell.width, source)
+		if i and d <= spell.width + GetHitBox(unit) and GetDistance(p) < spell.range + spell.width + GetHitBox(unit) then
+			return Prediction.User.State.ENEMY_IS_IMMOBILE, Vector(p), d
+		end
+		--[[ local wp = Prediction.WayPointManager.GetWayPoints(unit)
+		if #wp == 0 then
+			return Prediction.User.State.WILL_MISS, nil
+		end
+		TODO: cone | circular
+		if spell.type == "cone" then
+			chance, pos, info = Prediction.Core.PredictCone(unit, range, speed, delay, width, source)
+		elseif spell.type == "linear " then
+			chance, pos, info = Prediction.Core.PredictLinear(unit, range, speed, delay, width, source)
+		else
+			chance, pos, info = Prediction.Core.PredictCircular(unit, range, speed, delay, width, source)
+		end]]
+		return Prediction.Core.PredictLinear(unit, spell.range, spell.speed, spell.delay, spell.width, source)
 	end
 
-	function Prediction.Core.GetLatestWaypoints(hero)
-		local waypoints = Prediction.Vars.WayPoints[GetNetworkID(hero)]
-		local last = nil
-		for i, waypoint in ipairs(waypoints) do
-			last = waypoint
-		end
-		return last
+	function Prediction.Core.PredictPos(unit, delay)
+		return Vector(target)+Vector(Prediction.WayPointManager.GetDirection(unit)):normalized()*delay*GetMoveSpeed(unit)
 	end
 
-	function Prediction.Core.GetSecondLatestWaypoints(hero)
-		local waypoints = Prediction.Vars.WayPoints[GetNetworkID(hero)]
-		local last = nil
-		local slast = nil
-		for i, waypoint in ipairs(waypoints) do
-			slast = last and last or nil
-			last = waypoint
-		end
-		return slast
+	function Prediction.Core.PredictCone(unit, range, speed, delay, width, source)
 	end
 
-	function Prediction.WayPointManager.GetSimulatedWayPoints(object, fromT, toT)
-		local wayPoints, fromT, toT = Prediction.WayPointManager.GetWayPoints(object), fromT or 0, toT or math.huge
-		local invisDur = (not object.visible and WayPointVisibility[object.networkID]) and GetGameTimer() - WayPointVisibility[object.networkID] or ((not object.visible and not WayPointVisibility[object.networkID]) and math.huge or 0)
-		fromT = fromT + invisDur
-		local tTime, fTime, result = 0, 0, {}
-		for i = 1, #wayPoints - 1 do
-			local A, B = wayPoints[i], wayPoints[i + 1]
-			local dist = GetDistance(A, B)
-			local cTime = dist / object.ms
-			if tTime + cTime >= fromT then
-				if #result == 0 then
-					fTime = fromT - tTime
-					result[1] = { x = A.x + object.ms * fTime * ((B.x - A.x) / dist), y = A.y + object.ms * fTime * ((B.y - A.y) / dist) }
-				end
-				if tTime + cTime >= toT then
-					result[#result + 1] = { x = A.x + object.ms * (toT - tTime) * ((B.x - A.x) / dist), y = A.y + object.ms * (toT - tTime) * ((B.y - A.y) / dist) }
-					fTime = fTime + toT - tTime
-					break
-				else
-					result[#result + 1] = B
-					fTime = fTime + cTime
-				end
-			end
-			tTime = tTime + cTime
-		end
-		if #result == 0 and (tTime >= toT or invisDur) then result[1] = wayPoints[#wayPoints] end
-		return result, fTime
+	function Prediction.Core.PredictCircular(unit, range, speed, delay, width, source)
 	end
 
-	function Prediction.WayPointManager.GetWayPoints(object)
-		local wayPoints, lineSegment, distanceSqr, fPoint = Prediction.WayPointManager.WayPoints[object.networkID], 0, math.huge, nil
-		if not wayPoints then return { x = object.x, y = object.z } end
-		for i = 1, #wayPoints - 1 do
-			local p1, _, _ = VectorPointProjectionOnLineSegment(wayPoints[i], wayPoints[i + 1], object)
-			local distanceSegmentSqr = GetDistanceSqr(p1, object)
-			if distanceSegmentSqr < distanceSqr then
-				fPoint = p1
-				lineSegment = i
-				distanceSqr = distanceSegmentSqr
-			else break
-			end
-		end
-		local result = { fPoint or { x = object.x, y = object.z } }
-		for i = lineSegment + 1, #wayPoints do
-			result[#result + 1] = wayPoints[i]
-		end
-		if #result == 2 and GetDistanceSqr(result[1], result[2]) < 400 then result[2] = nil end
-		Prediction.WayPointManager.WayPoints[object.networkID] = result
-		return result
-	end
-
-	function Prediction.Core.GetWaypointsSet(hero, mtime)
-		local waypoints = Prediction.Vars.WayPoints[GetNetworkID(hero)]
-		local set = {}
-		for i, waypoint in ipairs(waypoints) do
-			if (GetGameTimer() - waypoint.time) <= mtime then
-				table.insert(set, waypoint)
-			end
-		end
-		return set
+	function Prediction.Core.PredictLinear(unit, range, speed, delay, width, source)
+        local Position = Prediction.Core.PredictPos(unit, GetDistance(source,unit)/speed+delay)
+        print(GetDistance(Position, unit))
+        return GetDistance(Position, source) < range + width + GetHitBox(unit) and Prediction.User.State.WILL_HIT or Prediction.User.State.WILL_MISS, Position
 	end
 
 	function Prediction.Core.pythag(x,y)
-		return math.sqrt(math.pow(x,2) + math.pow(y,2))
+		return math.sqrt(x*x + y*y)
 	end
 
+	function Prediction.WayPointManager.GetWayPoints(unit, from, to)
+		local from, to = from or 10, to or 0
+		local result = {}
+		if not Prediction.Vars.WayPoints[GetNetworkID(unit)] then Prediction.Vars.WayPoints[GetNetworkID(unit)] = {} end
+		for i, waypoint in pairs(Prediction.Vars.WayPoints[GetNetworkID(unit)]) do
+			if (GetGameTimer() - waypoint.time) < from and (GetGameTimer() - waypoint.time) > to then
+				table.insert(result, waypoint)
+			end
+		end
+		return result
+	end
+
+	function Prediction.WayPointManager.RemoveOldWayPoints(unit)
+		local result = {}
+		for i, waypoint in pairs(Prediction.Vars.WayPoints[GetNetworkID(unit)]) do
+			if (GetGameTimer() - waypoint.time) >= 10 then
+				table.remove(Prediction.Vars.WayPoints[GetNetworkID(unit)], i)
+			end
+		end
+		return result
+	end
+
+	function Prediction.WayPointManager.IsVisible(unit)
+		return true
+	end
+
+	function Prediction.WayPointManager.GetPath(unit)
+		local wp = Prediction.WayPointManager.GetWayPoints(unit)
+		local wpn, lastI = #wp, 0
+		local result, result2 = {}, {}
+		for I=wpn, 1, -1 do
+			if lastI > 0 and lastI > wp[I].time then
+				break
+			end
+			lastI = wp[I].time
+			table.insert(result, wp[I].pos)
+		end
+		for I=#result, 1, -1 do
+			table.insert(result2, result[I])
+		end
+		return result2
+	end
+
+	function Prediction.WayPointManager.GetSimulatedPath(unit)
+		local wp = Prediction.WayPointManager.GetPath(unit)
+		return wp
+	end
+
+	function Prediction.WayPointManager.GetDirection(unit)
+		local path = Prediction.WayPointManager.GetPath(unit)
+		local unit = Vector(unit)
+		if #path == 0 then
+			return unit
+		else
+			local path = path[#path]
+			return Vector(path.x - unit.x, path.y - unit.y, path.z - unit.z)
+		end
+	end
+
+	Prediction.User.State = {
+		WILL_MISS = 0,
+		INVISIBLE = 1,
+		WILL_COLLIDE = 2,
+		WILL_HIT = 3,
+		ENEMY_IS_SLOWED = 4,
+		ENEMY_IS_DASHING = 5,
+		ENEMY_IS_IMMOBILE = 6,
+		ENEMY_IS_STUPID = 7,
+	}
+	Prediction.User.StateToString = {
+		[0] = "WILL_MISS",
+		[1] = "INVISIBLE",
+		[2] = "WILL_COLLIDE",
+		[3] = "WILL_HIT",
+		[4] = "ENEMY_IS_SLOWED",
+		[5] = "ENEMY_IS_DASHING",
+		[6] = "ENEMY_IS_IMMOBILE",
+		[7] = "ENEMY_IS_STUPID"
+	}
+
 	function Prediction.User.OnDash(callback, range, speed, delay, width, source)
+		local callback, range, speed, delay, width, source = callback, range, speed, delay, width, source
+		if type(range) == "table" then
+			source = speed
+			speed = range.speed
+			delay = range.delay
+			width = range.width
+			range = range.range
+		end
 		Prediction.Vars.DashCallbacks[#Prediction.Vars.DashCallbacks + 1] = {callback = callback, range = range, speed = speed, delay = delay, width = width, source = source or myHero}
 	end
 
 	function Prediction.User.OnImmobile(callback, range, speed, delay, width, source)
+		local callback, range, speed, delay, width, source = callback, range, speed, delay, width, source
+		if type(range) == "table" then
+			source = speed
+			speed = range.speed
+			delay = range.delay
+			width = range.width
+			range = range.range
+		end
 		Prediction.Vars.StunCallbacks[#Prediction.Vars.StunCallbacks + 1] = {callback = callback, range = range, speed = speed, delay = delay, width = width, source = source or myHero}
 	end
 
@@ -338,13 +370,16 @@ do
 		local canHasStun = Prediction.Vars.Stunned[GetNetworkID(unit)];
 		if canHasStun and canHasStun.endT and canHasStun.endT > GetGameTimer() then
 			local remainingTime = canHasStun.endT - GetGameTimer()
+			if canHasStun.pos and canHasStun.midT < remainingTime then
+				Prediction.Vars.Stunned[GetNetworkID(unit)].pos = GetOrigin(unit)
+			end
 			if remainingTime > delay then
-				return true, 0
+				return true, 0, canHasStun.pos or GetOrigin(unit)
 			else
-				return true, GetMoveSpeed(unit) * (delay - remainingTime)
+				return true, GetMoveSpeed(unit) * (delay - remainingTime), canHasStun.pos or GetOrigin(unit)
 			end
 		end
-		return false, GetMoveSpeed(unit) * delay
+		return false, GetMoveSpeed(unit) * delay, GetOrigin(unit)
 	end
 
 	function Prediction.User.Interface.IsUnitSlowed(unit, range, speed, delay, width, source)
@@ -397,15 +432,24 @@ do
 		return false, nil, math.huge
 	end
 
-	do
+	function Prediction.User.Interface.Collision(startP, endP, spell)
+		return Prediction.Core.Collision(startP, endP, spell)
+	end
 
+	function Prediction.User.Interface.CollisionH(startP, endP, spell)
+		return Prediction.Core.Collision(startP, endP, spell, Obj_AI_Hero)
+	end
+
+	function Prediction.User.Interface.CollisionM(startP, endP, spell)
+		return Prediction.Core.Collision(startP, endP, spell, Obj_AI_Minion)
+	end
+
+	do
 		for name, callback in pairs(Prediction.Callback) do
 			_G["On"..name](callback)
 		end
-
 		Prediction.Vars.Config:Info("i1", "Loaded!")
 		Prediction.Vars.Config:Info("i2", "More menu options soon")
-
 	end
 
 	class "Spell"
@@ -423,13 +467,16 @@ do
 	end
 
 	function Spell:Predict(unit, source)
-		return nil--Prediction.Core.Predict(unit, self.range, self.speed, self.delay, self.width, self.collision, self.type, source or myHero)
+		return Prediction.Core.Predict(unit, self, source or myHero)
 	end
 
 	_G.IPrediction = {
 		IsUnitDashing = Prediction.User.Interface.IsUnitDashing,
 		IsUnitSlowed = Prediction.User.Interface.IsUnitSlowed,
 		IsUnitStunned = Prediction.User.Interface.IsUnitStunned,
+		CollisionM = Prediction.User.Interface.CollisionM,
+		CollisionH = Prediction.User.Interface.CollisionH,
+		Collision = Prediction.User.Interface.Collision,
 		Prediction = function(data)
 			if not (data.range and data.speed and data.delay and data.width) then print("Please specify spelldata!") end
 			local spell = Spell({range = data.range, speed = data.speed, delay = data.delay, width = data.width, type = data.type, collision = type(data.collision) == "number" and data.collision or data.collision and 0 or math.huge, callback = callback})
@@ -437,6 +484,8 @@ do
 			return spell
 		end,
 		OnDash = Prediction.User.OnDash,
-		OnImmobile = Prediction.User.OnImmobile
+		OnImmobile = Prediction.User.OnImmobile,
+		State = Prediction.User.State,
+		StateToString = Prediction.User.StateToString,
 	}
 end
